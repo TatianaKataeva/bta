430543c0c652d52db400890e04e08dacab7ab55f
annaegorovaa <anju.egorova@gmail.com>
2018-09-12 18:17:10 -0700

    replace values with variables in tests for login functionality email field

--- test/login-functionality.js
+++ test/login-functionality.js
@@ -16,21 +16,21 @@ describe('Login Functionality', function () {
 
         it('User can enter 22 symbols', function () {
             browser.clearElement(locator.email);
-            browser.setValue(locator.email,'a'.repeat(22));
+            browser.setValue(locator.email,'a'.repeat(data.midLength));
             let input = browser.getValue(locator.email);
             assert.equal(input.length, data.midLength);
         });
 
         it('User can enter 45 symbols', function () {
             browser.clearElement(locator.email);
-            browser.setValue(locator.email,'n'.repeat(45));
+            browser.setValue(locator.email,'n'.repeat(data.maxLength));
             let input = browser.getValue(locator.email);
             assert.equal(input.length, data.maxLength);
         });
 
         it('User can\'t enter 46 symbols', function () {
             browser.clearElement(locator.email);
-            browser.setValue(locator.email,'x'.repeat(46));
+            browser.setValue(locator.email,'x'.repeat(data.maxLength + 1));
             let input = browser.getValue(locator.email);
             assert.equal(input.length, data.maxLength);
         });
