a41072f9c2bb2d21b6e6a155b7273d00e01fdb29
iskotar <40219954+iskotar@users.noreply.github.com>
2018-09-15 21:27:00 -0700

    Test cases for Summary

--- test/bug-report-form-edit-mode.js
+++ test/bug-report-form-edit-mode.js
@@ -4,15 +4,6 @@ const locator = require('../data/locator');
 
 describe('bug-report-form-edit-mode', function () {
 
-  // describe('Page has the list of the same titles as in Default State', function () {
-  //
-  //   it('Field Summary', function () {
-  //     let fontFamily = browser.getCssProperty(locator.bugReportEditMode.input, 'font-family');
-  //     assert.equal(fontFamily, data.fontFamily, 'Summary font family is incorrect');
-  //   });
-  //
-  // });
-
   describe('Summary', function () {
 
     it('is a single line input field', function () {
@@ -56,73 +47,4 @@ describe('bug-report-form-edit-mode', function () {
     assert.equal(align, data.TextAlign, 'Summary text is not aligned to the left')
     });
   });
-
-  // describe('Steps to reproduce', function () {
-  //
-  // it('accept multiline input', function () {
-  //   browser.url('/');
-  //   let elType = browser.getCssProperty(locator.bugReportEditMode.txtArea);
-  //   assert.equal(elType.getTagName(), data.tagTextArea, 'Summary is not a single line input field'); //И заканчивается тут
-  // });
-  //
-  //   it('text area font family: Segoe UI', function () {
-  //   let fontFamily = browser.getCssProperty(locator.bugReportEditMode.txtArea, 'font-family').value;
-  //   assert.equal(fontFamily, data.globalHeader.fontFamily, 'Steps to reproduce font family is incorrect');
-  //   });
-  //
-  //   it(' font size: 16px', function () {
-  //     let fontSize = browser.getCssProperty(locator.bugReportEditMode.txtArea, 'font-size').value;
-  //     assert.equal(fontSize, data.summaryFontSize, 'Summary font size is incorrect');
-  //   });
-  //
-  //   it('font weight is 400', function () {
-  //     let fontWeight = browser.getCssProperty(locator.bugReportEditMode.txtArea, 'font-weight').value;
-  //     assert.equal(fontWeight, data.globalHeader.fontWeight, 'Summary font weight is incorrect');
-  //   });
-  //
-  //   it('font color is #495057', function () {
-  //     let fontColor = browser.getCssProperty(locator.bugReportEditMode.txtArea, 'color').value;
-  //     assert.equal(fontColor, data.summaryFontColor, 'Summary color is incorrect');
-  //   });
-  //
-  //   it('text is aligned to the left', function () {
-  //     let align = browser.getCssProperty(locator.bugReportEditMode.txtArea, 'text-align').value;
-  //     assert.equal(align, data.summaryTextAlign, 'Summary text is not aligned to the left')
-  //   });
-  // });
-
-  // describe('Actual Result', function () {
-
-    // it('is a single line input field', function () {
-    //   browser.url('/');
-    //   let elType = browser.getCssProperty(locator.input);
-    //   assert.equal(elType.getTagName(), data.tagSummary, 'Summary is not a single line input field'); //И заканчивается тут
-    // });
-
-  //   it('font family is Segoe UI', function () {
-  //     let fontFamily = browser.getCssProperty(locator.input, 'font-family').value;
-  //     assert.equal(fontFamily, data.globalHeader.fontFamily, 'Steps to reproduce font family is incorrect');
-  //   });
-  //
-  //   it('font size: 16px', function () {
-  //     let fontSize = browser.getCssProperty(locator.input, 'font-size').value;
-  //     assert.equal(fontSize, data.summaryFontSize, 'Summary font size is incorrect');
-  //   });
-  //
-  //   it('font weight is 400', function () {
-  //     let fontWeight = browser.getCssProperty(locator.input, 'font-weight').value;
-  //     assert.equal(fontWeight, data.globalHeader.fontWeight, 'Summary font weight is incorrect');
-  //   });
-  //
-  //   it('font color is #495057', function () {
-  //     let fontColor = browser.getCssProperty(locator.input, 'color').value;
-  //     assert.equal(fontColor, data.summaryFontColor, 'Summary color is incorrect');
-  //   });
-  //
-  //   it('text is aligned to the left', function () {
-  //     let align = browser.getCssProperty(locator.input, 'text-align').value;
-  //     assert.equal(align, data.summaryTextAlign, 'Summary text is not aligned to the left')
-  //   });
-  // });
-
 });
